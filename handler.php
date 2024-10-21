25f81ffdb64e9b21c808bbf067b88c9f93dded55
Enrico-Tabilin <enrico.tabilin@stu.ocsb.ca>
2024-10-21 20:25:35 +0000

    Complete index.html and handler.php

--- handler.php
+++ handler.php
@@ -4,19 +4,54 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
+    <title>Circle Calculations</title>
   </head>
 
   <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
+    <!-- Always shows a header, even in smaller screens. -->
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <!-- Title -->
+          <span class="mdl-layout-title">Circle Calculator</span>
+          <!-- Add spacer, to align navigation to the right -->
+          <div class="mdl-layout-spacer"></div>
+          <!-- Navigation. We hide it in small screens. -->
+          <nav class="mdl-navigation mdl-layout--large-screen-only">
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+          </nav>
+        </div>
+      </header>
+      <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Circle Calculator</span>
+        <nav class="mdl-navigation">
+          <a class="mdl-navigation__link" href="">Link</a>
+          <a class="mdl-navigation__link" href="">Link</a>
+          <a class="mdl-navigation__link" href="">Link</a>
+          <a class="mdl-navigation__link" href="">Link</a>
+        </nav>
+      </div>
+      <main class="mdl-layout__content">
+        <div class="page-content">
+          <?php
+          $diameter = "";
+          if ( isset( $_POST['diameter'] ) ){
+            $diameter = $_POST['diameter'];
+          }
+          $radius = (float)$diameter/2;
+          $circumference = $radius**2*pi();
+          $area = 2*pi()*$radius;
+          echo "<h1>Calculations</h1>\n";
+          echo "<p>The radius is: ". $radius ." units</p>\n";
+          echo "<p>The circumference is: ". $circumference ." units</p>\n";
+          echo "<p>The area is: " . $area . " units</p>\n";
+          ?>
+        </div>
+      </main>
+    </div>
     
   </body>
   
